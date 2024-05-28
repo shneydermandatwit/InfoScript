3b6178fca4ef855b627e5745dc5415d43cf603b1
David Shneyderman <shneydermand@wit.edu>
2024-05-28 19:11:57 -0400

    made basic navbar

--- frontend/info-script/src/app/app.routes.ts
+++ frontend/info-script/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register'
+    },
+    
+];
